e4a33533f78dc58fb840992907252c369e1d71f6
franjefriten <fj.friastenza@gmail.com>
2025-04-24 21:29:02 +0200

    agente creado con smolagents e integrado a la app de streamlit

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from sqlalchemy import text
 
+from smolagents import CodeAgent, AzureOpenAIServerModel
+from models.agente_rag import *
+
 import os, re
 from datetime import datetime
 import time
@@ -22,43 +25,6 @@ load_dotenv()
 URI_TO_DB = f"postgresql+psycopg://{os.getenv("POSTGRES_USER")}:{os.getenv("POSTGRES_PASSWORD")}@db:5432/{os.getenv("POSTGRES_DB")}"
 
 
-
-# def _query_the_huggingface_rag_agent():
-#     print(
-#         """
-#         ¡Hola! Soy ACCAP, tu Asistente para Consultas de Convocatorias y Ayudas Públicas.
-#         Tengo en mi una base de datos de varias consultas con esta información.
-#         - nombre: nombre oficial de la convocatoria
-#         - entidad: entidad que organiza la convocatoria
-#         - fecha_publicacion: fecha de publicacion de la convocatoria
-#         - fecha_inicio: fecha de inicio del período de aplicabilidad de la convocatoria
-#         - fecha_final: fecha final del período de aplicabilidad de la convocatoria
-#         - presupuesto: monto económico que ofrece la convocatoria
-#         - localidad: localidad donde se concede la ayuda
-#         - url: enlace para más información
-#         Por favor, mantente fiel a la estructura de arriba y emplea los nombres como
-#         están redactados. En caso de que no sea capaz de hayar alguna, realizaré una búsqueda semántica
-#         con el mensaje de tu consulta y te mostraré las que creo que más se aproximan a lo que pides.
-#         Si quieres salir, solo escribe 'exit'
-#         """
-#     )
-#     query = input()
-#     agente = CodeAgent(
-#         tools=[
-#             PerformSemanticSearchQuerying(),
-#             PerformStandardSQLQuerying()
-#         ],
-#         model=HfApiModel(
-#             model_id="deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B",
-#             token=os.getenv("HUGGING_FACE_TOKEN")
-#         )
-#     )
-#     while query.lower() != "exit":
-#         agente.run(
-#             query,
-#         )
-
-
 endpoint = os.getenv("AZURE_AI_AGENT_ENDPOINT")
 api_key = os.getenv("AZURE_AI_AGENT_API_KEY")
 logging.getLogger("openai").setLevel(logging.WARNING)
@@ -70,16 +36,7 @@ transfomers_logging.disable_progress_bar()
 transfomers_logging.set_verbosity_warning()
 logging.getLogger("urllib3").setLevel(logging.WARNING)
 
-client = AzureOpenAI(  
-    azure_endpoint=endpoint,  
-    api_key=api_key,  
-    api_version="2025-01-01-preview",
-)
-
 st.title("ACCAP")
-
-if "openai_model" not in st.session_state:
-    st.session_state["openai_model"] = "gpt-4o-mini"
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
@@ -122,93 +79,61 @@ for message in st.session_state.messages:
 
 if query := st.chat_input("Haz tu consulta"):
     st.session_state.messages.append({"role": "user", "content": query})
+    
     with st.chat_message("user"):
         st.markdown(query)
-
+    
     with st.chat_message("assistant"):
-        chat_prompt = [
-            {
-                "role": "system",
-                "content": [
-                    {
-                        "type": "text",
-                        "text": 
-                            """Este es un asistente que debe, dada una consulta humana 
-                            y un esquema de tabla de base de datos PostgreSQL, ser capaz de decidir si
-                                1- Convertir el texto a humano a lenguaje SQL para realizar la consulta. En este caso, solo debe devolver el código solicitado como texto plano.
-                                2- Obtener las palabras claves para realizar búsqueda semántica. En este caso, solo debe devolver una cadena de texto con las palabras separadas por comas.
-                            El esquema de base de datos es el siguiente:
-                            table_name: call_data
-                                    - id: INTEGER PRIMARY KEY
-                                    - nombre: VARCHAR(255) NOT NULLABLE
-                                    - entidad: VARCHAR(255)
-                                    - fecha_publicacion: DATETIME
-                                    - fecha_inicio: DATETIME
-                                    - fecha_final: DATETIME
-                                    - presupuesto: FLOAT
-                                    - localidad: VARCHAR(255)
-                                    - url: VARCHAR(255) NOT NULLABLE
-                                    - keywords: VECTOR(384)
-                            Solo se debe proveer una de las opciones consideradas
-                            A continuación, el usuario te ofrece su consulta:
+        agente = CodeAgent(
+            tools=[
+                PerformQuery()
+            ],
+            model=AzureOpenAIServerModel(
+                model_id="gpt-4o-mini",
+                azure_endpoint=endpoint,
+                api_key=api_key,
+                api_version="2025-01-01-preview",
+                client_kwargs={
+                    "max_tokens":800,  
+                    "temperature":0.7,  
+                    "top_p":0.95,  
+                    "frequency_penalty":0,  
+                    "presence_penalty":0,
+                    "stop":None,
+                    "stream":False                    
+                }
+            )
+        )
+        
+        try:
 
-                            """ + query
-                    }
-                ]
-            }
-        ]
-        completion = client.chat.completions.create(  
-            model="gpt-4o-mini",
-            messages=chat_prompt,
-            max_tokens=800,  
-            temperature=0.7,  
-            top_p=0.95,  
-            frequency_penalty=0,  
-            presence_penalty=0,
-            stop=None,
-            stream=False  
-        )
-        mensaje = completion.choices[0].message.content
-        if 'sql' in mensaje or "SELECT" in mensaje:
-            query = re.sub(r"```sql\s*|```", "", mensaje).strip()
-            engine = create_engine(URI_TO_DB)
-            Session = sessionmaker(bind=engine)
-            with Session() as session:
-                result = session.execute(text(query)).fetchall()
-        else:
-            model = SentenceTransformer("jaimevera1107/all-MiniLM-L6-v2-similarity-es")
-            embedding: np.ndarray = np.mean(model.encode(sentences=query.split(","), show_progress_bar=False), axis=0)
-            engine = create_engine(URI_TO_DB)
-            Session = sessionmaker(bind=engine)
-            with Session() as session:
-                result = session.execute(
-                    text("""
-                    SELECT *, 
-                        1 - (keywords <=> CAST(:embedding AS vector(384))) as similarity
-                    FROM call_data
-                    ORDER BY similarity DESC
-                    LIMIT 5
-                    """), 
-                    {"embedding": embedding.tolist()}
-                ).fetchall()
-
-        df = pd.DataFrame([dict(row._mapping) for row in result])
-        mensaje_respuesta =  """
-            ¡Por supuesto! Aquí tienes todos los resultados que he encontrado
-            que se pueden ajustar a tu solicitud. Los tienes abajo en una tabla
-            """
-        st.write(mensaje_respuesta)
-        columns_to_show = st.multiselect(
-            "Select columns",
-            options=df.columns.to_list(),
-            default=df.columns.to_list()
-        )
-        st.dataframe(df[columns_to_show])
-        st.write(
-            """
-            ¡No dudes en volver a consultarme!
-            """
-        )
-        st.session_state.messages.append({"role": "assistant", "content": mensaje_respuesta, "df": df})
+            response = agente.run(query)
+            
+            if hasattr(response, "tool_result") and isinstance(response.tool_result, pd.DataFrame):
+                df = response.tool_result
+                mensaje = "¡Aquí están los resultados:"
+                
+                st.markdown(mensaje)
+                cols = st.multiselect(
+                    "Columnas a mostrar",
+                    options=df.columns,
+                    default=df.columns.tolist()
+                )
+                st.dataframe(df[cols])
+                
+                st.session_state.messages.append({
+                    "role": "assistant",
+                    "content": mensaje,
+                    "df": df
+                })
+            else:
+                st.markdown(response)
+                st.session_state.messages.append({
+                    "role": "assistant",
+                    "content": response
+                })
+                
+        except Exception as e:
+            st.error(f"Error: {str(e)}")
             
 
--- models/agente_rag.py
+++ models/agente_rag.py
@@ -1,8 +1,14 @@
-# from sqlalchemy.orm import sessionmaker, Session
-# from sqlalchemy import create_engine, text
-# from sentence_transformers import SentenceTransformer
-# import numpy as np
-# import os
+from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy import create_engine, text
+from sentence_transformers import SentenceTransformer
+import numpy as np
+import pandas as pd
+import os
+from typing import Optional
+from smolagents import Tool
+from azure.core.credentials import AzureKeyCredential
+from azure.ai.textanalytics import TextAnalyticsClient
+import streamlit as st
 
 # class QueryReasoner:
 #     def __init__(self):
@@ -69,116 +75,77 @@
 #         pass
 
 
-# class PerformStandardSQLQuerying(Tool):
-#     name = "perform_standard_sql_querying"
-#     description = """
-#     This function is used to do standard sql querying (postgres dialect). Returns a string representation
-#     of the result. The table is named 'call_data' and has data about subsidies and public calls for economic aid. The
-#     description is as follows:
-#         Columns:
-#         - id: INTEGER PRIMARY KEY
-#         - nombre: VARCHAR(255) NOT NULLABLE
-#             oficial name of the call
-#         - entidad: VARCHAR(255)
-#             entity that organizes the call
-#         - fecha_publicacion: DATETIME
-#             publication date of the call
-#         - fecha_inicio: DATETIME
-#             start of application period of the call
-#         - fecha_final: DATETIME
-#             end of application period of the call
-#         - presupuesto: FLOAT
-#             budget of the call
-#         - localidad: VARCHAR(255)
-#             place where the call is organized
-#         - url: VARCHAR(255) NOT NULLABLE
-#             url to information about the call
-#         - keywords: VECTOR(384)
-#             a vector representing an embedding of the call's keywords
-#     """
-#     inputs = {
-#         "query": {
-#             "type": "string",
-#             "description": "The query to perform. This should be correct SQL."
-#         }
-#     }
-#     output_type = "string"
+class PerformQuery(Tool):
+    name = "text_to_sql"
+    description = """
+    This function will receive a sql query that uses the postgres dialect and pgvector and
+    is generated from a user query.
+    Alongside with that, it will return a set of keywords from the user query
+    The schema of the database table 'call_data' to query to is this one:
+        Columns:
+        - id: INTEGER PRIMARY KEY
+        - nombre: VARCHAR(255) NOT NULLABLE
+            oficial name of the call
+        - entidad: VARCHAR(255)
+            entity that organizes the call
+        - fecha_publicacion: DATETIME
+            publication date of the call
+        - fecha_inicio: DATETIME
+            start of application period of the call
+        - fecha_final: DATETIME
+            end of application period of the call
+        - presupuesto: FLOAT
+            budget of the call
+        - localidad: VARCHAR(255)
+            place where the call is organized
+        - url: VARCHAR(255) NOT NULLABLE
+            url to information about the call
+        - bases: VARCHAR(255)
+            url to information about the legal basis of the call
+        - beneficiario: VARCHAR(255)
+            beneficiaries elegible to apply
+        - tipo: VARCHAR(255)
+            type of economic aid such as "subvencion", "concesion", etc.
+        - keywords: VECTOR(384)
+            a vector representing an embedding of the call's keywords
+    The query should have a parameter `:embedding` (e.g., `1 - (keywords <=> :embedding)`) 
+    if case cosine similarity search is used.
+    """
+    inputs = {
+        "query": {
+            "type": str,
+            "description": "The sql query that will be performed"
+        },
+        "list_of_keywords": {
+            "type": list,
+            "description": "List of the keywords",
+            "element_type": {"type": "str"},
+            "required": False
+        }
+    }
 
 
-#     def forward(
-#         self,
-#         query: str,
-#     ):
-#         uri: str = f"postgresql+psycopg://{os.getenv("POSTGRES_USER")}:{os.getenv("POSTGRES_PASSWORD")}@localhost:5432/{os.getenv("POSTGRES_DB")}",
-#         output = "Estas son las entradas que más se pueden ajustar a tu consulta:"
-#         engine = create_engine(uri)
-#         Session = sessionmaker(bind=engine)
-#         with Session() as session:
-#             result = session.execute(text(query))
-#             for row in result:
-#                 output += "\n\t*" + str(row)
-#         output += "\n¡Espero que te sirva!"
-#         return output
+    def forward(self, query: str, list_of_keywords: Optional[list[str]] = None) -> pd.DataFrame:
+        params = {}
+        if ":embedding" in query:
+            params["embedding"] = self._get_keywords_and_generate_embedding(list_of_keywords)
+        
+        return self._execute_query(query, params)
 
+    def _get_keywords_and_generate_embedding(self, list_of_keywords: list[str]) -> list:
+        """Generate a 384-dim embedding if needed."""
+        embedding_model = SentenceTransformer("jaimevera1107/all-MiniLM-L6-v2-similarity-es")
+        return embedding_model.encode(list_of_keywords, convert_to_tensor=False).tolist()
 
-# class PerformSemanticSearchQuerying(Tool):
-#     name = "perform_semantic_search_querying"
-#     description = """
-#     This function is used to do sql querying (postgres dialect) through semantic search. Returns a string representation
-#     of the result. The table is named 'call_data' and has data about subsidies and public calls for economic aid. The
-#     description is as follows:
-#         Columns:
-#         - id: INTEGER PRIMARY KEY
-#         - nombre: VARCHAR(255) NOT NULLABLE
-#             oficial name of the call
-#         - entidad: VARCHAR(255)
-#             entity that organizes the call
-#         - fecha_publicacion: DATETIME
-#             publication date of the call
-#         - fecha_inicio: DATETIME
-#             start of application period of the call
-#         - fecha_final: DATETIME
-#             end of application period of the call
-#         - presupuesto: FLOAT
-#             budget of the call
-#         - localidad: VARCHAR(255)
-#             place where the call is organized
-#         - url: VARCHAR(255) NOT NULLABLE
-#             url to information about the call
-#         - keywords: VECTOR(384)
-#             a vector representing an embedding of the call's keywords
-#     """
-#     inputs = {
-#         "keywords": {
-#             "type": "array",
-#             "description": "python list of strings that contains the main keywords og the user query"
-#         },
-#     }
-#     output_type = "string"
-
-
-#     def forward(
-#         self,
-#         keywords: list[str],
-#     ):
-#         uri: str = f"postgresql+psycopg://{os.getenv("POSTGRES_USER")}:{os.getenv("POSTGRES_PASSWORD")}@localhost:5432/{os.getenv("POSTGRES_DB")}",
-#         output = "Estas son las entradas que más se pueden ajustar a tu consulta:"
-#         engine = create_engine(uri)
-#         Session = sessionmaker(bind=engine)
-#         model = SentenceTransformer("jaimevera1107/all-MiniLM-L6-v2-similarity-es")
-#         query_embedding = np.mean(model.encode(keywords) , axis=0)
-#         with Session() as session:
-#             result = session.execute(
-#                 text("""
-#                 SELECT id, nombre, entidad, 
-#                     1 - (keywords <=> :embedding) as similarity
-#                 FROM call_data
-#                 ORDER BY similarity DESC
-#                 LIMIT 10
-#                 """), 
-#                 {"embedding": query_embedding}
-#             ).fetchall()
-#             for row in result:
-#                 output += "\n\t*" + str(row)
-#         output += "\n¡Espero que te sirva!"
-#         return output
+    def _execute_query(self, query: str, params: dict) -> pd.DataFrame:
+        """Run the query with parameters."""
+        uri = f"postgresql+psycopg://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@db:5432/{os.getenv('POSTGRES_DB')}"
+        engine = create_engine(uri)
+        try:
+            with engine.connect() as conn:
+                result = conn.execute(text(query), params)
+                return pd.DataFrame([dict(row._mapping) for row in result])
+        except Exception as e:
+            st.error(f"Database Error: {e}")
+        finally:
+            engine.dispose()
